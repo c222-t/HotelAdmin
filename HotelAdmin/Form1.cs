6a25725531441e615f349ac769b1169fee576bd5
士尧 翟 <1417226344@qq.com>
2021-02-23 09:15:44 +0800

    翟士尧

--- HotelAdmin/Form1.cs
+++ HotelAdmin/Form1.cs
@@ -16,5 +16,10 @@ namespace HotelAdmin
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("翟士尧");
+        }
     }
 }
