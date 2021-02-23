34300c676e8f8eba0f8a9c513c79b980d2982b87
士尧 翟 <1417226344@qq.com>
2021-02-23 22:03:52 +0800

    房间布局

--- HotelDAL/RoomService.cs
+++ HotelDAL/RoomService.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
+using HotelModel;
 
 namespace HotelDAL
 {
@@ -18,17 +19,83 @@ namespace HotelDAL
         /// <returns></returns>
         public DataTable RoomTable(string leiXing="")
         {
-            StringBuilder sql = new StringBuilder("SELECT [RoomNumber],[Floor],[RoomType],[RoomStatus] FROM [RoomSchedules] where 1=1");
+            DataTable dt = HotelData.ConsignData.Tables["RoomSchedules"];
 
-            SqlParameter[] sp = null;
+            DataTable d = new DataTable();
+
             if (leiXing != "")
             {
-                sql.Append("RoomType=@RoomType");
-                sp = new SqlParameter[] {new SqlParameter("@RoomType", leiXing) };
+                for (int i=0;i< dt.Rows.Count;i++)
+                {
+                    if (dt.Rows [i]["RoomType"].ToString() == leiXing)
+                    {
+                        DataRow dr = dt.NewRow();
+                        dr["RoomNumber"] = dt.Rows[i]["RoomNumber"].ToString();
+                        dr["Floor"]= dt.Rows[i]["Floor"].ToString();
+                        dr["RoomType"] = dt.Rows[i]["RoomType"].ToString();
+                        dr["RoomStatus"]= dt.Rows[i]["RoomStatus"].ToString();
+                        d.Rows.Add(dr);
+                    }
+                }
             }
-
-            DataTable dt = db.GetTable(sql.ToString (),sp,"RoomTable");
             return dt;
+        }
+
+        /// <summary>
+        /// 为房间表插入一行数据并返回,需要提供房间对象
+        /// </summary>
+        /// <param name="room">插入的房间信息</param>
+        /// <returns></returns>
+        public DataTable RoomInsert(RoomSchedules room)
+        {
+            DataRow dr = HotelData.ConsignData.Tables["RoomSchedules"].NewRow();
+
+            dr["RoomNumber"] = room.RoomNumber;
+            dr["Floor"] = room.Floor;
+            dr["RoomType"] = room.RoomType.No;
+            dr["RoomStatus"] = room.RoomStatus.No;
+
+            HotelData.ConsignData.Tables["RoomSchedules"].Rows.Add(dr);
+
+            return HotelData.ConsignData.Tables["RoomSchedules"];
+        }
+
+        /// <summary>
+        /// 为房间表删除一行数据并返回，需要提供房间编号(名称)
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public DataTable RoomDelect(string name)
+        {
+            foreach (DataRow item in HotelData.ConsignData.Tables["RoomSchedules"].Rows)
+            {
+                if (item["RoomNumber"].ToString() == name)
+                {
+                    HotelData.ConsignData.Tables["RoomSchedules"].Rows.Remove(item);
+                    break;
+                }
+            }
+            return HotelData.ConsignData.Tables["RoomSchedules"];
+        }
+
+        /// <summary>
+        /// 为房间表修改一行数据并返回，需要提供房间编号(名称),以及修改的内容
+        /// </summary>
+        /// <param name="num"></param>
+        /// <param name="room"></param>
+        /// <returns></returns>
+        public DataTable RoomUpdate(string num,RoomSchedules room)
+        {
+            for (int i=0;i< HotelData.ConsignData.Tables["RoomSchedules"].Rows.Count;i++)
+            {
+                if (HotelData.ConsignData.Tables["RoomSchedules"].Rows[i]["RoomNumber"].ToString() == num)
+                {
+                    HotelData.ConsignData.Tables["RoomSchedules"].Rows[i]["RoomType"] = room.RoomType.No;
+                    HotelData.ConsignData.Tables["RoomSchedules"].Rows[i]["RoomStatus"] = room.RoomStatus.No;
+                    break;
+                }
+            }
+            return HotelData.ConsignData.Tables["RoomSchedules"];
         }
 
 
