62b903dcf5376a11f2da0e2acc5b392b03d3d0ca
3207792078@qq.com <3207792078@qq.com>
2021-02-23 11:04:05 +0800

    管理员方法框架

--- HotelAdmin/MainInterface.Designer.cs
+++ HotelAdmin/MainInterface.Designer.cs
@@ -30,18 +30,18 @@ namespace HotelAdmin
         private void InitializeComponent()
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.InquiringThe = new System.Windows.Forms.ToolStripMenuItem();
-            this.客户管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.RoomKeeper = new System.Windows.Forms.ToolStripMenuItem();
-            this.员工管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.系统设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.关于ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.商品管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.Billing = new System.Windows.Forms.ToolStripMenuItem();
-            this.TopUp = new System.Windows.Forms.ToolStripMenuItem();
             this.CheckOut = new System.Windows.Forms.ToolStripMenuItem();
             this.Book = new System.Windows.Forms.ToolStripMenuItem();
-            this.挂起ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TopUp = new System.Windows.Forms.ToolStripMenuItem();
+            this.InquiringThe = new System.Windows.Forms.ToolStripMenuItem();
+            this.RoomKeeper = new System.Windows.Forms.ToolStripMenuItem();
+            this.CustomerCRM = new System.Windows.Forms.ToolStripMenuItem();
+            this.PersonnelManage = new System.Windows.Forms.ToolStripMenuItem();
+            this.CommodityManager = new System.Windows.Forms.ToolStripMenuItem();
+            this.SystemSetup = new System.Windows.Forms.ToolStripMenuItem();
+            this.Suspend = new System.Windows.Forms.ToolStripMenuItem();
+            this.AsFor = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -54,89 +54,101 @@ namespace HotelAdmin
             this.TopUp,
             this.InquiringThe,
             this.RoomKeeper,
-            this.客户管理ToolStripMenuItem,
-            this.员工管理ToolStripMenuItem,
-            this.商品管理ToolStripMenuItem,
-            this.系统设置ToolStripMenuItem,
-            this.挂起ToolStripMenuItem,
-            this.关于ToolStripMenuItem});
+            this.CustomerCRM,
+            this.PersonnelManage,
+            this.CommodityManager,
+            this.SystemSetup,
+            this.Suspend,
+            this.AsFor});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(1184, 25);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
-            // InquiringThe
-            // 
-            this.InquiringThe.Name = "InquiringThe";
-            this.InquiringThe.Size = new System.Drawing.Size(68, 21);
-            this.InquiringThe.Text = "营业查询";
-            // 
-            // 客户管理ToolStripMenuItem
-            // 
-            this.客户管理ToolStripMenuItem.Name = "客户管理ToolStripMenuItem";
-            this.客户管理ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
-            this.客户管理ToolStripMenuItem.Text = "客户管理";
-            // 
-            // RoomKeeper
-            // 
-            this.RoomKeeper.Name = "RoomKeeper";
-            this.RoomKeeper.Size = new System.Drawing.Size(68, 21);
-            this.RoomKeeper.Text = "房间管理";
-            // 
-            // 员工管理ToolStripMenuItem
-            // 
-            this.员工管理ToolStripMenuItem.Name = "员工管理ToolStripMenuItem";
-            this.员工管理ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
-            this.员工管理ToolStripMenuItem.Text = "员工管理";
-            // 
-            // 系统设置ToolStripMenuItem
-            // 
-            this.系统设置ToolStripMenuItem.Name = "系统设置ToolStripMenuItem";
-            this.系统设置ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
-            this.系统设置ToolStripMenuItem.Text = "系统设置";
-            // 
-            // 关于ToolStripMenuItem
-            // 
-            this.关于ToolStripMenuItem.Name = "关于ToolStripMenuItem";
-            this.关于ToolStripMenuItem.Size = new System.Drawing.Size(32, 21);
-            this.关于ToolStripMenuItem.Text = "关";
-            // 
-            // 商品管理ToolStripMenuItem
-            // 
-            this.商品管理ToolStripMenuItem.Name = "商品管理ToolStripMenuItem";
-            this.商品管理ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
-            this.商品管理ToolStripMenuItem.Text = "商品管理";
-            // 
             // Billing
             // 
             this.Billing.Name = "Billing";
             this.Billing.Size = new System.Drawing.Size(44, 21);
             this.Billing.Text = "开单";
-            // 
-            // TopUp
-            // 
-            this.TopUp.Name = "TopUp";
-            this.TopUp.Size = new System.Drawing.Size(44, 21);
-            this.TopUp.Text = "充值";
+            this.Billing.Click += new System.EventHandler(this.Billing_Click);
             // 
             // CheckOut
             // 
             this.CheckOut.Name = "CheckOut";
             this.CheckOut.Size = new System.Drawing.Size(44, 21);
             this.CheckOut.Text = "结账";
+            this.CheckOut.Click += new System.EventHandler(this.CheckOut_Click);
             // 
             // Book
             // 
             this.Book.Name = "Book";
             this.Book.Size = new System.Drawing.Size(44, 21);
             this.Book.Text = "预定";
+            this.Book.Click += new System.EventHandler(this.Book_Click);
             // 
-            // 挂起ToolStripMenuItem
+            // TopUp
             // 
-            this.挂起ToolStripMenuItem.Name = "挂起ToolStripMenuItem";
-            this.挂起ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
-            this.挂起ToolStripMenuItem.Text = "挂起";
+            this.TopUp.Name = "TopUp";
+            this.TopUp.Size = new System.Drawing.Size(44, 21);
+            this.TopUp.Text = "充值";
+            this.TopUp.Click += new System.EventHandler(this.TopUp_Click);
+            // 
+            // InquiringThe
+            // 
+            this.InquiringThe.Name = "InquiringThe";
+            this.InquiringThe.Size = new System.Drawing.Size(68, 21);
+            this.InquiringThe.Text = "营业查询";
+            this.InquiringThe.Click += new System.EventHandler(this.InquiringThe_Click);
+            // 
+            // RoomKeeper
+            // 
+            this.RoomKeeper.Name = "RoomKeeper";
+            this.RoomKeeper.Size = new System.Drawing.Size(68, 21);
+            this.RoomKeeper.Text = "房间管理";
+            this.RoomKeeper.Click += new System.EventHandler(this.RoomKeeper_Click);
+            // 
+            // CustomerCRM
+            // 
+            this.CustomerCRM.Name = "CustomerCRM";
+            this.CustomerCRM.Size = new System.Drawing.Size(68, 21);
+            this.CustomerCRM.Text = "客户管理";
+            this.CustomerCRM.Click += new System.EventHandler(this.CustomerCRM_Click);
+            // 
+            // PersonnelManage
+            // 
+            this.PersonnelManage.Name = "PersonnelManage";
+            this.PersonnelManage.Size = new System.Drawing.Size(68, 21);
+            this.PersonnelManage.Text = "员工管理";
+            this.PersonnelManage.Click += new System.EventHandler(this.PersonnelManage_Click);
+            // 
+            // CommodityManager
+            // 
+            this.CommodityManager.Name = "CommodityManager";
+            this.CommodityManager.Size = new System.Drawing.Size(68, 21);
+            this.CommodityManager.Text = "商品管理";
+            this.CommodityManager.Click += new System.EventHandler(this.CommodityManager_Click);
+            // 
+            // SystemSetup
+            // 
+            this.SystemSetup.Name = "SystemSetup";
+            this.SystemSetup.Size = new System.Drawing.Size(68, 21);
+            this.SystemSetup.Text = "系统设置";
+            this.SystemSetup.Click += new System.EventHandler(this.SystemSetup_Click);
+            // 
+            // Suspend
+            // 
+            this.Suspend.Name = "Suspend";
+            this.Suspend.Size = new System.Drawing.Size(44, 21);
+            this.Suspend.Text = "挂起";
+            this.Suspend.Click += new System.EventHandler(this.Suspend_Click);
+            // 
+            // AsFor
+            // 
+            this.AsFor.Name = "AsFor";
+            this.AsFor.Size = new System.Drawing.Size(44, 21);
+            this.AsFor.Text = "关于";
+            this.AsFor.Click += new System.EventHandler(this.AsFor_Click);
             // 
             // MainInterface
             // 
@@ -159,16 +171,16 @@ namespace HotelAdmin
 
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem InquiringThe;
-        private System.Windows.Forms.ToolStripMenuItem 客户管理ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem CustomerCRM;
         private System.Windows.Forms.ToolStripMenuItem RoomKeeper;
-        private System.Windows.Forms.ToolStripMenuItem 员工管理ToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem 系统设置ToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem 关于ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem PersonnelManage;
+        private System.Windows.Forms.ToolStripMenuItem SystemSetup;
+        private System.Windows.Forms.ToolStripMenuItem AsFor;
         private System.Windows.Forms.ToolStripMenuItem Billing;
         private System.Windows.Forms.ToolStripMenuItem CheckOut;
         private System.Windows.Forms.ToolStripMenuItem Book;
         private System.Windows.Forms.ToolStripMenuItem TopUp;
-        private System.Windows.Forms.ToolStripMenuItem 商品管理ToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem 挂起ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem CommodityManager;
+        private System.Windows.Forms.ToolStripMenuItem Suspend;
     }
 }
--- HotelAdmin/MainInterface.cs
+++ HotelAdmin/MainInterface.cs
@@ -25,5 +25,113 @@ namespace HotelAdmin
         {
             
         }
+        /// <summary>
+        /// 开单
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Billing_Click(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// 结账
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CheckOut_Click(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// 预定
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Book_Click(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// 充值
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TopUp_Click(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// 营业查询
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void InquiringThe_Click(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// 房间管理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RoomKeeper_Click(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// 客户管理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CustomerCRM_Click(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// 员工管理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PersonnelManage_Click(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// 商品管理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CommodityManager_Click(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// 系统设置
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SystemSetup_Click(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// 挂起
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Suspend_Click(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// 关于
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AsFor_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
