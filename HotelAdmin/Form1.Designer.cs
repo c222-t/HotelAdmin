5e0242f09c483bfbe0f9882f62673f3c020fc472
士尧 翟 <1417226344@qq.com>
2021-02-23 10:40:53 +0800

    登录完成

--- HotelAdmin/Form1.Designer.cs
+++ HotelAdmin/Form1.Designer.cs
@@ -30,7 +30,61 @@ namespace HotelAdmin
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(122, 188);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(129, 21);
+            this.txtName.TabIndex = 0;
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(122, 237);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.Size = new System.Drawing.Size(129, 21);
+            this.txtPwd.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(119, 168);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "用户名";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(119, 218);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "密码";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(150, 307);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "登录";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // Form1
             // 
@@ -39,14 +93,26 @@ namespace HotelAdmin
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(740, 452);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.txtName);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- HotelAdmin/Form1.cs
+++ HotelAdmin/Form1.cs
@@ -7,19 +7,60 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using HotelBLL;
+using HotelModel;
 
 namespace HotelAdmin
 {
     public partial class Form1 : Form
     {
+        AdministratorManager atm = new AdministratorManager();
         public Form1()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// 登录时判断是否输入
+        /// </summary>
+        /// <returns></returns>
+        public bool PanDuan()
+        {
+            if (txtName.Text.Trim() == "")
+            {
+                MessageBox.Show("请输入用户名");
+                txtName.Focus();
+                return false;
+            }
+            else if (txtPwd.Text.Trim() == "")
+            {
+                MessageBox.Show("请输入密码");
+                txtPwd.Focus();
+                return false;
+            }
+            return true;
+        }
+
+
+
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (PanDuan())
+            {
+                if (atm.AdministrTable(txtPwd.Text.Trim(), txtName.Text.Trim()) > 0)
+                {
+                    MessageBox.Show("登录成功");
+                }
+                else
+                {
+                    MessageBox.Show("登录失败，用户名或密码错误");
+                }
+            }
 
 
         }
