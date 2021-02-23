3b7ea0ec118f67231be70b5bc690cdb23cba60a5
3207792078@qq.com <3207792078@qq.com>
2021-02-23 09:51:42 +0800

    111

--- HotelAdmin/Form1.cs
+++ HotelAdmin/Form1.cs
@@ -19,7 +19,7 @@ namespace HotelAdmin
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("123");
+            MessageBox.Show("144423");
 
         }
     }
