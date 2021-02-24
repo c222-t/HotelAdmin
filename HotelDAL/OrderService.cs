f3b37ff9e231ed7ec7aca3dfc7288ce4ce3d1553
3207792078@qq.com <3207792078@qq.com>
2021-02-24 09:52:32 +0800

    提交订单业务处理

--- HotelDAL/OrderService.cs
+++ HotelDAL/OrderService.cs
@@ -15,31 +15,129 @@ namespace HotelDAL
     /// </summary>
     public class OrderService
     {
-        
-        private delegate DataTable GetOrderTables(DataTable dataTable, StatementTable table);
-
         /// <summary>
         /// 搜索满足条件或全部的订单信息
         /// </summary>
-        /// <param name="statement">查询的订单信息</param>
+        /// <param name="order">查询的订单信息</param>
         /// <returns>返回满足条件的订单列表</returns>
-        public DataTable SeekOrderRecord(StatementTable statement)
+        public DataTable SeekOrderRecord(StatementTable order)
         {
+            // 创建临时数据表获取所有订单记录
             DataTable table = HotelData.ConsignData.Tables["StatementTable"];
 
-            StringBuilder sql = new StringBuilder(
-                "select orderNumber, IDCard, TotalConsumption, CheckInTime, CheckoutTime, OperationManaer, Status from StatementTable where 1 = 1"
-            );
-            
-
-            if (!statement.OrderNumber.Equals(null)) {
-                sql.Append(" and orderNumber = " + statement.OrderNumber);
-            } if (!statement.IDCard.Equals(string.Empty)) {
-                sql.Append(" and IDCard = " + statement.IDCard);
-            } if (!statement.Status.Number.Equals(null)) {
-                sql.Append(" and Status = " + statement.Status.Number);
+            // 根据指定的条件执行相应的查询方式
+            if (order.OrderNumber.Equals(string.Empty)) {               // 是否根据编号查询订单
+                CompareStatementNo(table, order);
             }
-            return table;
+            else if (!order.IDCard.Equals(string.Empty)) {              // 是否根据身份证查询订单
+                CompareStatementIDCard(table, order);
+            }
+            else if (!order.Status.Number.Equals(0)) {                  // 是否根据订单状态查询订单
+                CompareStatementStatus(table, order);
+            }
+            return table;                                               // 返回得到的订单记录
+        }
+        /// <summary>
+        /// 删除指定的订单记录
+        /// </summary>
+        /// <param name="order">要删除的订单信息</param>
+        public void DeleteOrderRecord(StatementTable order)
+        {
+            // 遍历系统临时数据库搜索满足条件的记录
+            foreach (DataRow row in HotelData.ConsignData.Tables["StatementTable"].Rows)
+            {
+                if (order.OrderNumber.Equals(row["orderNumber"])) {     // 删除指定条件的记录
+                    HotelData.ConsignData.Tables["StatementTable"].Rows.Remove(row);
+                    break;
+                }
+            }
+        }
+        /// <summary>
+        /// 修改指定的订单记录 (修改时必须传入订单的所有信息)
+        /// </summary>
+        /// <param name="order">要修改的订单信息</param>
+        public void AmendOrderRecord(StatementTable order)
+        {
+            // 遍历系统临时数据库搜索满足条件的记录
+            foreach (DataRow row in HotelData.ConsignData.Tables["StatementTable"].Rows)
+            {
+                if (order.OrderNumber.Equals(row["orderNumber"]))       // 修改指定的条件的记录
+                {
+                    row[1] = order.IDCard;
+                    row[2] = order.TotalConsumption;
+                    row[3] = order.PaymentMethod;
+                    row[4] = order.CheckInTime.ToString();
+                    row[5] = order.CheckoutTime.ToString();
+                    row[6] = order.OperationManager;
+                    row[7] = order.Status.Number;
+                    break;
+                }
+            }
+        }
+        /// <summary>
+        /// 添加需要的订单记录
+        /// </summary>
+        /// <param name="order">需要添加的订单记录</param>
+        public void AddOrderRecord(StatementTable order)
+        {
+            if (order.OrderNumber.Equals(string.Empty))                 // 判断记录是否满足条件
+            {
+                DataRow row = HotelData.ConsignData.Tables["StatementTable"].NewRow();
+                row[0] = order.OrderNumber;
+                row[1] = order.IDCard;
+                row[2] = order.TotalConsumption;
+                row[3] = order.PaymentMethod;
+                row[4] = order.CheckInTime.ToString();
+                row[5] = order.CheckoutTime.ToString();
+                row[6] = order.OperationManager;
+                row[7] = order.Status.Number;
+                HotelData.ConsignData.Tables["StatementTable"].Rows.Add(row);
+            }
+        }
+        /// <summary>
+        /// 根据订单编号查询指定记录
+        /// </summary>
+        /// <param name="dataTable">订单记录表</param>
+        /// <param name="order">要查询的订单条件</param>
+        /// <returns>返回满足条件的订单记录</returns>
+        public DataTable CompareStatementNo(DataTable dataTable, StatementTable order)
+        {
+            // 根据指定订单编号查询记录
+            var table = from row in dataTable.AsEnumerable()
+                        where row["orderNumber"].Equals(order.OrderNumber.ToString())
+                        select row;
+
+            return table.CopyToDataTable();                             // 返回得到的记录
+        }
+        /// <summary>
+        /// 根据订单身份证查询指定记录
+        /// </summary>
+        /// <param name="dataTable">订单记录表</param>
+        /// <param name="order">要查询的订单条件</param>
+        /// <returns>返回满足条件的订单记录</returns>
+        public DataTable CompareStatementIDCard(DataTable dataTable, StatementTable order)
+        {
+            // 根据指定身份证查询订单记录
+            var table = from row in dataTable.AsEnumerable()
+                        where row["IDCard"].Equals(order.IDCard)
+                        select row;
+
+            return table.CopyToDataTable();                             // 返回得到的记录
+        }
+        /// <summary>
+        /// 根据订单状态查询指定记录
+        /// </summary>
+        /// <param name="dataTable">订单记录表</param>
+        /// <param name="order">要查询的订单条件</param>
+        /// <returns>返回满足条件的订单记录</returns>
+        public DataTable CompareStatementStatus(DataTable dataTable, StatementTable order)
+        {
+            // 查询满足指定状态的订单记录
+            var table = from row in dataTable.AsEnumerable()
+                        where row["Number"].Equals(order.Status.Number)
+                        select row;
+
+            return table.CopyToDataTable();                             // 返回得到的记录
         }
     }
 }
