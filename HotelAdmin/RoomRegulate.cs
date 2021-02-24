fa4fee266457c355c8d60260b5b23066418d0592
士尧 翟 <1417226344@qq.com>
2021-02-24 14:50:23 +0800

    房间管理

--- HotelAdmin/RoomRegulate.cs
+++ HotelAdmin/RoomRegulate.cs
@@ -7,19 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using HotelBLL;
+using HotelModel;
 
 namespace HotelAdmin
 {
     public partial class RoomRegulate : Form
     {
+        RoomTypeManager rtm = new RoomTypeManager();
         public RoomRegulate()
         {
-            InitializeComponent();
+            InitializeComponent();      
         }
 
         private void RoomRegulate_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("asd");
+            DataTable dt = rtm.TypeTable();
+
+            foreach (DataRow item in dt.Rows)
+            {
+                TreeNode tn = new TreeNode();
+                tn.Text = item["Name"].ToString().Trim();
+                tn.Tag = item;
+
+                tvCaiDan.Nodes[0].Nodes.Add(tn);
+            }
+            
         }
     }
 }
