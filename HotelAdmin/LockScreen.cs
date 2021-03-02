b6df8ed4d62cb71cc8ec21eb7e537341b0be6c3c
士尧 翟 <1417226344@qq.com>
2021-03-02 15:00:17 +0800

    挂机

---
+++ HotelAdmin/LockScreen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HotelAdmin
+{
+    public partial class LockScreen : Form
+    {
+        public LockScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (txtPwd.Text == "1")
+            {
+                this.Close();
+            }
+
+        }
+    }
+}
