2098f5e261635c4d5a071a0acac499e9da40f5e4
士尧 翟 <1417226344@qq.com>
2021-03-02 15:43:37 +0800

    1

---
+++ HotelAdmin/OrderTable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HotelAdmin
+{
+    public partial class OrderTable : Form
+    {
+        public OrderTable()
+        {
+            InitializeComponent();
+        }
+
+        private void OrderTable_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
