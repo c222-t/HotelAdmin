1b4269b2f64d9c4cb78c5d438e1dcf1d4b76af7c
士尧 翟 <1417226344@qq.com>
2021-03-02 15:55:44 +0800

    员工管理

--- HotelAdmin/StaffManagement.Designer.cs
+++ HotelAdmin/StaffManagement.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // StaffManagement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(862, 488);
+            this.Name = "StaffManagement";
             this.Text = "StaffManagement";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
